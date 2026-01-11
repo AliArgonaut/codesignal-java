5baa74a50f579b6dd97fb4916cc656bc1acdd3d2
lloyd aleister bonds <lloydabonds@gmail.com>
2026-01-11 18:53:04 -0500

    Solution 4

--- 4.java
+++ 4.java
@@ -1,3 +1,23 @@
-class Solution {
+import java.util.ArrayList;
 
+class Solution {
+    public static void main(String[] args) {
+        ArrayList<String> packingList = new ArrayList<>();
+        packingList.add("clothes");
+        packingList.add("toothbrush");
+        packingList.add("passport");
+        packingList.add("camera");
+
+        String itemToCheck = "passport";
+        boolean isItemPacked = false;
+        int itemIndex = -1;
+
+        if (packingList.contains(itemToCheck)) {
+            isItemPacked = true;
+            itemIndex = packingList.indexOf(itemToCheck);
+        }
+
+        System.out.println("Is the item packed? " + isItemPacked);
+        System.out.println("Item index: " + itemIndex);
+    }
 }
