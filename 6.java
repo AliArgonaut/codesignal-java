01035b524f5b9c16213344738fad7db4068e4000
lloyd aleister bonds <lloydabonds@gmail.com>
2026-01-14 18:30:07 -0500

    solution 6 started

---
+++ 6.java
@@ -0,0 +1,13 @@
+class Solution {
+    public static void main(String[] args) {
+        String missionName = "Sort";
+
+        // TODO: Print the original first and last character of the mission name
+
+        System.out.println(missionName[0]);
+
+        // TODO: Create a new string with the first letter as 'P' and last letter as 'k'
+
+        // TODO: Print the updated mission name
+    }
+}
